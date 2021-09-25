f8cf830ecd601cc814d1bfbbe6e54ff5974e8ec5
ihdazul <ihdazul123@gmail.com>
2021-09-26 05:02:36 +0700

    first commit

---
+++ RplaFire.java
@@ -0,0 +1,14 @@
+public class RplaFire {
+	
+	public static void main(String[] args){
+	 System.out.println("RPLA Fire!");
+	 System.out.println("Daftar Player RPLA Fire :");
+	 System.out.println("Zandira = Jungler");
+	 System.out.println("Sexpa = Offlaner");
+	 System.out.println("imamsakti = Tank");
+	 System.out.println("Nairobi = Sidelaner");
+	 System.out.println("HadRap = Support/Sidelaner");
+	 System.out.println("Hiiragi. = Support/Jungler");
+	 System.out.println("Waldi = Tank");
+	 }
+	}
